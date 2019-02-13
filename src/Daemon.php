69209bf25540404abb3de8b176cad28e197e3017
yunbo <kernelman79@gmail.com>
2019-02-14 03:56:58 +0800

    Optimization code.

--- src/Daemon.php
+++ src/Daemon.php
@@ -23,47 +23,49 @@ class Daemon
     private $user           = 'www';
     private $fileName       = null;
     private $class          = null;         // current run class
-    private $className      = null;         // current run class name
-    private $action         = null;         // current run action(method)
+    private $daemonName     = null;         // current run class name
+    private $timer          = null;         // run timer
+    private $timerAction    = null;         // run timer action
+    private $running        = null;         // running flag
     private static $signal  = 0;            // Set current signal
 
     const   IS_DOWN         = 'The process is not running';
     const   IS_RUN          = 'The process is running';
     const   IS_STOP         = 'The process has stoped';
     const   IS_START        = 'The process has started';
+    const   IS_RELOAD       = 'The process has reloaded';
+    const   IS_FAILED       = 'the process failed';
 
     /**
      * Daemon constructor.
-     * @param $class
-     * @param $action
-     * @param string $pidParams
+     *
+     * Daemon constructor.
+     * @param null $timer
+     * @param null $timerAction
+     * @param bool $running
      * @param null $user
-     * @throws InvalidArgumentException
      * @throws NotFoundException
      */
-    public function __construct($class, $action, $pidParams = '', $user = null) {
-        if(!is_object($class)) {
-            throw new InvalidArgumentException('$class params type error, only for the object');
+    public function __construct($timer = null, $timerAction = null, $running = false, $user = null) {
+
+        $this->running      = $running;
+        $this->timer        = $timer;
+        $this->timerAction  = $timerAction;
+        $this->class        = $timer->callClass;
+
+        if(!property_exists($timer, 'callClass')) {
+            throw new NotFoundException('callClass property on the Timer');
         }
 
-        if(!method_exists($class, $action)) {
-            throw new NotFoundException('$action on the $class');
-        }
-
-        if(!is_string($action)) {
-            throw new InvalidArgumentException('$action params type error, only for the string');
-        }
-
-        $this->class    = $class;
-        $this->action   = $action;
-        $this->className= Strings::trimStrString(get_class($this->class), '\\');
+        $this->daemonName   = Strings::trimStrString(get_class($this->class), '\\');
 
         // The pid file save path.
-        $this->fileName = $this->pidFile . $this->className . $pidParams . '.pid';
+        $this->fileName = $this->pidFile . $this->daemonName . '.pid';
         // Set exec users, default to www.
         if ($user != null) {
             $this->user = $user;
         }
+
         $this->signal();
     }
 
@@ -72,7 +74,7 @@ class Daemon
      *
      * @param $signal
      */
-    public static function setSignal($signal){
+    public static function setSignal($signal) {
         self::$signal = $signal;
     }
 
@@ -100,6 +102,7 @@ class Daemon
     public static function isReload() {
         $flag = false;
         if(Daemon::getSignal() == SIGHUP) {
+
             Daemon::resetSignal();
             $flag = true;
         }
@@ -117,20 +120,47 @@ class Daemon
     }
 
     /**
-     * Run current class
+     * Run timer tick
+     *
+     * @throws InvalidArgumentException
+     * @throws NotFoundException
      */
     private function run() {
 
-        while (true) {
-            sleep(10);
-            $this->class->run();
+        if (!$this->running) {
+            $this->timerTick();
+
+        } else {
+            while (true) {
+                $this->timerTick();
+            }
+        }
+    }
+
+    /**
+     * Run timer tick action
+     *
+     * @throws InvalidArgumentException
+     * @throws NotFoundException
+     */
+    private function timerTick() {
+        if ($this->timer != null && $this->timerAction != null) {
+
+            if(!method_exists($this->timer, $this->timerAction)) {
+                throw new NotFoundException('$timerAction on the $timer');
+            }
+
+            $this->timer->{$this->timerAction}();
+
+        } else {
+            throw new InvalidArgumentException('timer or timerAction required');
         }
     }
 
     /**
      * Fork the user process become the daemon
      */
-    private function daemonFork(){
+    private function daemonFork() {
         $pid = pcntl_fork();
         if ($pid == -1) {
             die('Fork(1) failed!' . PHP_EOL);
@@ -160,7 +190,7 @@ class Daemon
      *
      * @return bool
      */
-    private function setUser(){
+    private function setUser() {
         $result = false;
         if ($this->user == null){
             return true;
@@ -190,7 +220,7 @@ class Daemon
      * @throws \Exceptions\UnWritableException
      */
     private function createPidfile() {
-        FileStore::save($this->fileName, posix_getpid(), false, true);
+        return FileStore::save($this->fileName, posix_getpid(), false, true);
     }
 
     /**
@@ -202,20 +232,25 @@ class Daemon
      * @throws \Exceptions\UnReadableException
      * @throws \Exceptions\UnWritableException
      */
-    private function stop(){
-        if (file_exists($this->fileName)) {
+    private function stop() {
+        if (FileStore::checkFile($this->fileName)) {
+
             $pid = FileStore::get($this->fileName);
             posix_kill($pid, SIGUSR1);
             $del = FileStore::deleteFile($this->fileName);
 
             if ($del) {
-                echo $this->className . ': ' . self::IS_STOP . PHP_EOL;
+                echo $this->daemonName . ': ' . self::IS_STOP . PHP_EOL;
+                return true;
+
+            } else {
+                echo $this->daemonName . ': ' . 'Stop ' . self::IS_FAILED . PHP_EOL;
             }
 
-        } else {
-            echo self::IS_DOWN . PHP_EOL;
+            return false;
         }
 
+        echo $this->daemonName . ': ' . self::IS_DOWN . PHP_EOL;
         return false;
     }
 
@@ -223,21 +258,28 @@ class Daemon
      * Start the process
      */
     private function start() {
-        if (file_exists($this->fileName)) {
-            echo $this->className . ': ' . self::IS_RUN . PHP_EOL;
+        if (FileStore::checkFile($this->fileName)) {
+
+            echo $this->daemonName . ': ' . self::IS_RUN . PHP_EOL;
             exit(0);
         }
 
         $this->daemonFork();
-        $this->createPidfile();
-        echo $this->className . ': ' . self::IS_START . PHP_EOL;
-        $this->run();
+        $pid = $this->createPidfile();
+
+        if ($pid) {
+            $this->run();
+            echo $this->daemonName . ': ' . self::IS_START . PHP_EOL;
+
+        } else {
+            echo $this->daemonName . ': ' . 'Start ' . self::IS_FAILED . PHP_EOL;
+        }
     }
 
     /**
      * Restart the process
      */
-    private function restart(){
+    private function restart() {
         $this->stop();
         $this->start();
     }
@@ -245,14 +287,24 @@ class Daemon
     /**
      * Reload the process
      */
-    private function reload(){
-        if (file_exists($this->fileName)) {
-            $pid = FileStore::get($this->fileName);
-            posix_kill($pid, SIGHUP);
-            return true;
+    private function reload() {
+        if (FileStore::checkFile($this->fileName)) {
+
+            $pid    = FileStore::get($this->fileName);
+            $reload = posix_kill($pid, SIGHUP);
+
+            if ($reload) {
+                echo $this->daemonName . ': ' . self::IS_RELOAD. PHP_EOL;
+                return true;
+
+            } else {
+                echo $this->daemonName . ': ' . 'Reload ' . self::IS_FAILED . PHP_EOL;
+            }
+
+            return false;
         }
 
-        echo self::IS_DOWN . PHP_EOL;
+        echo $this->daemonName . ': ' . self::IS_DOWN . PHP_EOL;
         return false;
     }
 
@@ -263,7 +315,7 @@ class Daemon
      * @throws \Exceptions\UnReadableException
      * @throws \Exceptions\UnWritableException
      */
-    public function call($action){
+    public function call($action) {
         switch ($action){
             case 'start':
                 $this->start();
